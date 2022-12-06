16a30a374def6f630728437733703a29621b3dcf
huangjiaheng <huang_jia_heng@163.com>
2022-12-06 15:30:29 +0800

    change to dart 14.4

--- lib/src/method_channel/utils/exception.dart
+++ lib/src/method_channel/utils/exception.dart
@@ -8,15 +8,13 @@ import 'package:flutter/services.dart';
 
 /// Catches a [PlatformException] and converts it into a [FirebaseException] if
 /// it was intentionally caught on the native platform.
-Never convertPlatformException(Object exception, StackTrace stackTrace) {
+Exception convertPlatformException(Object exception, [StackTrace? stackTrace]) {
   if (exception is! Exception || exception is! PlatformException) {
-    Error.throwWithStackTrace(exception, stackTrace);
+    // ignore: only_throw_errors
+    throw exception;
   }
 
-  Error.throwWithStackTrace(
-    platformExceptionToFirebaseException(exception, stackTrace),
-    stackTrace,
-  );
+  return platformExceptionToFirebaseException(exception, stackTrace);
 }
 
 /// Converts a [PlatformException] into a [FirebaseException].
@@ -25,9 +23,8 @@ Never convertPlatformException(Object exception, StackTrace stackTrace) {
 /// `details` of the exception exist. Firebase returns specific codes and messages
 /// which can be converted into user friendly exceptions.
 FirebaseException platformExceptionToFirebaseException(
-  PlatformException platformException,
-  StackTrace stackTrace,
-) {
+    PlatformException platformException,
+    [StackTrace? stackTrace]) {
   Map<String, String>? details = platformException.details != null
       ? Map<String, String>.from(platformException.details)
       : null;
@@ -41,9 +38,8 @@ FirebaseException platformExceptionToFirebaseException(
   }
 
   return FirebaseException(
-    plugin: 'firebase_crashlytics',
-    code: code,
-    message: message,
-    stackTrace: stackTrace,
-  );
+      plugin: 'firebase_crashlytics',
+      code: code,
+      message: message,
+      stackTrace: stackTrace);
 }
